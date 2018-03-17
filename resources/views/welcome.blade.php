16fb7a2f78b66abbbdafa1a1364c93845d089a48
gitchao5288 <jiangchao456@foxmail.com>
2018-03-17 15:47:31 +0800

    文本内容换成懒熊闲置

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,8 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                    Bear
+                    懒熊闲置
                 </div>
 
                 <div class="links">
