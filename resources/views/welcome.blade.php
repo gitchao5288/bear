82dd468b5a21fa04c72b5ffb5b3b69823c5ed5e7
mycherish <determined_xw@126.com>
2018-03-17 15:36:51 +0800

    在首页添加bear

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,6 +80,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
+                    Bear
                 </div>
 
                 <div class="links">
